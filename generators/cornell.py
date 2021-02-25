Fixed buggy data: pcd0165r, Fixed normalization of data

generators/cornell.py
@@ -8,6 +8,14 @@ import math
 
 image_sizes = [512, 640, 768, 896, 1024, 1280, 1408]
 
+## DATASET VARIABLES
+## Accurate
+# GRASP_MEAN = [2.41776629e+02, 2.91271791e+02, 1.66374625e-01, 1.09318981e-01, 2.73294631e+01, 4.62548973e+01]
+# GRASP_STD = [26.8794504,  42.2205626,   0.71402385,  0.67122186, 11.29402456, 21.29478423]
+## Rounded off
+GRASP_MEAN = [241.8, 291.3, 0.1664, 0.1093, 27.33, 46.25]
+GRASP_STD = [26.88,  42.22,   0.7140,  0.6712, 11.29, 21.29]
+
 # Image Preprocessing
 def central_square_crop(image, side=None):
     h, w = image.shape[0], image.shape[1]
@@ -75,30 +83,31 @@ def bbox_to_grasp(box, side_length=512.0):
     sin_t = (box[3] - box[1]) / w
     cos_t = (box[2] - box[0]) / w
     # return x, y, tan_t, h, w
-    fact = 1.0
-    delta = 0.1
-    return x/side_length*fact + delta, y/side_length*fact + delta, (sin_t+1)/2*fact + delta, (cos_t+1)/2*fact + delta, h/side_length*fact + delta, w/side_length*fact + delta
+    x = (x-GRASP_MEAN[0])/GRASP_STD[0]
+    y = (y-GRASP_MEAN[1])/GRASP_STD[1]
+    sin_t = (sin_t-GRASP_MEAN[2])/GRASP_STD[2]
+    cos_t = (cos_t-GRASP_MEAN[3])/GRASP_STD[3]
+    h = (h-GRASP_MEAN[4])/GRASP_STD[4]
+    w = (w-GRASP_MEAN[5])/GRASP_STD[5]
+    return x, y, sin_t, cos_t, h, w
+    # return x/side_length*fact + delta, y/side_length*fact + delta, (sin_t+1)/2*fact + delta, (cos_t+1)/2*fact + delta, h/side_length*fact + delta, w/side_length*fact + delta
     # return 2*x/side_length-1, 2*y/side_length-1, sin_t, cos_t, h/side_length, w/side_length
 
 def grasp_to_bbox(x, y, sin_t, cos_t, h, w, side_length=512.0):
 # def grasp_to_bbox(x, y, tan_t, h, w, side_length=512.0):
-    # x = (x+1) * side_length/2.0
-    # y = (y+1) * side_length/2.0
-    # h = (h) * side_length
-    # w = (w) * side_length
-    # sin_t /= 10.0
-    # cos_t /= 10.0
-    fact = 1.0
-    delta = 0.1
-    sin_t = (sin_t-delta) * 2 /fact - 1
-    cos_t = (cos_t-delta) * 2 /fact - 1
+    x = x * GRASP_STD[0] + GRASP_MEAN[0]
+    y = y * GRASP_STD[1] + GRASP_MEAN[1]
+    sin_t = sin_t * GRASP_STD[2] + GRASP_MEAN[2]
+    cos_t = cos_t * GRASP_STD[3] + GRASP_MEAN[3]
+    h = h * GRASP_STD[4] + GRASP_MEAN[4]
+    w = w * GRASP_STD[5] + GRASP_MEAN[5]
     norm_fact = (sin_t**2 + cos_t**2) ** 0.5
     sin_t = sin_t / norm_fact
     cos_t = cos_t / norm_fact
-    x = (x-delta) * side_length /fact
-    y = (y-delta) * side_length /fact
-    h = (h-delta) * side_length /fact
-    w = (w-delta) * side_length /fact
+    # x = (x-delta) * side_length /fact
+    # y = (y-delta) * side_length /fact
+    # h = (h-delta) * side_length /fact
+    # w = (w-delta) * side_length /fact
     # theta = np.arctan(tan_t)
     # sin_t = np.sin(theta)
     # cos_t = np.cos(theta)
@@ -169,7 +178,6 @@ class CornellGenerator(Sequence):
                 bboxes = load_bboxes(bbox_file)
                 # Pick 1 random bbox
                 r = 8*np.random.randint(len(bboxes)/8)
-                r = min(2, int(len(bboxes)/8))
                 bbox = bboxes[r:r+8]
                 # Modify bbox acc to img scaling
                 for j in range(len(bbox)):

load_model.py
@@ -13,20 +13,20 @@ def allow_gpu_growth_memory():
     config.gpu_options.allow_growth = True
     _ = tf.Session(config = config)
 
-# Set before running
-allow_gpu_growth_memory()
+# # Set before running
+# allow_gpu_growth_memory()
 
-# Build Model
-model, prediction_model, all_layers = build_EfficientGrasp(0,
-                                                        num_classes = 10,
-                                                        num_anchors = 1,
-                                                        freeze_bn = not False,
-                                                        score_threshold = 0.5,
-                                                        print_architecture=False)
-print("Done!")
+# # Build Model
+# model, prediction_model, all_layers = build_EfficientGrasp(0,
+#                                                         num_classes = 10,
+#                                                         num_anchors = 1,
+#                                                         freeze_bn = not False,
+#                                                         score_threshold = 0.5,
+#                                                         print_architecture=False)
 
-# load pretrained weights
-model.load_weights('checkpoints/25_02_2021_16_17_24/phi_0_cornell_best_val_grasp_loss.h5', by_name=True)
+# # load pretrained weights
+# model.load_weights('checkpoints/25_02_2021_16_17_24/phi_0_cornell_best_val_grasp_loss.h5', by_name=True)
+# print("Done!")
 
 ## TEST ON SINGLE IMAGE
 filename = '/home/aby/Workspace/MTP/Datasets/Cornell/archive/06/pcd0600r.png'
@@ -46,13 +46,14 @@ with open(dataset+'/train.txt', 'r') as filehandle:
 #     # print(test_out)
 #     # print(type(test_out[0]))
 #     # print(test_out.shape)
-
+import math
+y_grasp_all = []
 # Train Print all Grasps
+max_grasps = 0
 for i, filename in enumerate(train_data):
     bbox_file = filename[:-5]+'cpos.txt'
     bboxes = load_bboxes(bbox_file)
     # Pick ALL bboxes
-    y_grasp_all = []
     for r in range(int(len(bboxes)/8)):
         q = 8*r
         bbox = bboxes[q:q+8]
@@ -65,13 +66,20 @@ for i, filename in enumerate(train_data):
 
         # Convert to Grasp
         grasp = bbox_to_grasp(bbox)
-
+        if math.isnan(grasp[0]):
+            print("Ghapla", i, " ", j)
+            print(filename)
         # Store 1 grasp
-        print("# Grasp Label ", i, " : ", r, " #", grasp)
+        # print("# Grasp Label ", i, " : ", r, " #", grasp)
         y_grasp_all.append(grasp)
     # Store all grasps for an image
     # y_grasp.append(y_grasp_all)
-
+# print(max_grasps)
+grasp_np = np.asarray(y_grasp_all)
+print(grasp_np.shape)
+# print(np.argwhere(np.isnan(grasp_np)))
+print(np.mean(grasp_np, axis = 0))
+print(np.std(grasp_np, axis = 0))
 exit()
 
 # weights_list = []

losses.py
@@ -60,6 +60,10 @@ def grasp_loss_bt(theta_err_wt = 1):
 # min loss among all grasps
 def grasp_loss_min(theta_err_wt = 1):
     def _grasp_loss_min(y_true, y_pred):
+        '''
+            y_true: Shape (B, Num_grasps, 6)
+            y_pred: (B, 6)
+        '''
         # For an image
         tot_loss = 0
         for i in range(y_true.shape[0]):
@@ -69,7 +73,7 @@ def grasp_loss_min(theta_err_wt = 1):
             for j in range(y_true.shape[1]):
                 y_true_grasp = y_true[i,j,:]
                 # Calculate loss
-                curr_loss = np.sum(np.square(y_true_grasp - y_pred_img))
+                curr_loss = np.sum(np.square(y_true_grasp - y_pred_img))  # Divide by 6 may be there. Check later
                 if curr_loss < min_loss:
                     min_loss = curr_loss
             tot_loss += min_loss

model.py
@@ -858,8 +858,8 @@ def apply_subnets_to_feature_maps(grasp_net, fpn_feature_maps, image_input, inpu
                                     # bias_regularizer=regularizers.l2(1e-4),
                                     # activity_regularizer=regularizers.l2(1e-5),
                                     name='regression_d2')(grasp_regression)
-    grasp_regression = layers.Flatten(name='regression_f')(grasp_regression)
-    grasp_regression = layers.Lambda(lambda x: tf.nn.swish(x), name='regression')(grasp_regression)
+    grasp_regression = layers.Flatten(name='regression')(grasp_regression)
+    # grasp_regression = layers.Lambda(lambda x: tf.nn.swish(x), name='regression')(grasp_regression)
     # rotation = [rotation_net([feature, i]) for i, feature in enumerate(fpn_feature_maps)]
     # rotation = layers.Concatenate(axis = 1, name='rotation')(rotation)
     